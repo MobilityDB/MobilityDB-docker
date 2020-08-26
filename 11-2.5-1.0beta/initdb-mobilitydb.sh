584fbded53cc1b777d2e512f5ab785f5cebfdf0d
mbakli <mohamed_bakli@aun.edu.eg>
2020-08-26 19:39:42 +0200

    Update docker-compose version

--- 11-2.5-1.0beta/initdb-mobilitydb.sh
+++ 11-2.5-1.0beta/initdb-mobilitydb.sh
@@ -6,6 +6,7 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 # Create the 'mobilitydb' extension in the mobilitydb database
+echo "Loading MobilityDB extension into mobilitydb"
 psql --dbname=mobilitydb <<- 'EOSQL'
 	SELECT POSTGIS_VERSION();
 	CREATE EXTENSION IF NOT EXISTS mobilitydb;
