4e80166a5bc2ae3fc3a2bea71b78e75c299fb159
mbakli <mohamed_bakli@aun.edu.eg>
2020-08-27 00:10:26 +0200

    Fix initdb file

--- 11-2.5-1.0beta/initdb-mobilitydb.sh
+++ 11-2.5-1.0beta/initdb-mobilitydb.sh
@@ -5,10 +5,15 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
+# Create the 'mobilitydb' db
+echo "Creating the mobilitydb database"
+"${psql[@]}" <<- 'EOSQL'
+CREATE DATABASE mobilitydb;
+EOSQL
+
 # Create the 'mobilitydb' extension in the mobilitydb database
 echo "Loading MobilityDB extension into mobilitydb"
-psql --dbname=mobilitydb <<- 'EOSQL'
-	SELECT POSTGIS_VERSION();
-	CREATE EXTENSION IF NOT EXISTS mobilitydb;
+"${psql[@]}" --dbname=mobilitydb <<- 'EOSQL'
+	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
 EOSQL
-done
+echo "Done!"
