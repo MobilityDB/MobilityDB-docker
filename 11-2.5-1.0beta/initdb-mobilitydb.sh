9df5d0755bebb996cc5022d40beb9f1da95959dc
mbakli <mohamed_bakli@aun.edu.eg>
2020-08-27 22:19:34 +0200

    Update Dockerfile

--- 11-2.5-1.0beta/initdb-mobilitydb.sh
+++ 11-2.5-1.0beta/initdb-mobilitydb.sh
@@ -6,14 +6,12 @@ set -e
 export PGUSER="$POSTGRES_USER"
 
 # Create the 'mobilitydb' db
-echo "Creating the mobilitydb database"
-"${psql[@]}" <<- 'EOSQL'
+psql <<- 'EOSQL'
 CREATE DATABASE mobilitydb;
 EOSQL
 
 # Create the 'mobilitydb' extension in the mobilitydb database
 echo "Loading MobilityDB extension into mobilitydb"
-"${psql[@]}" --dbname=mobilitydb <<- 'EOSQL'
+psql --dbname="mobilitydb" <<- 'EOSQL'
 	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
 EOSQL
-echo "Done!"
