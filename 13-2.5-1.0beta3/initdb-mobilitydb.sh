e3f357b76576823d05a391e3390457a173c14bcd
Mohamed Bakli <mohamed_bakli@aun.edu.eg>
2021-03-02 10:20:58 +0100

    Docker for beta3

---
+++ 13-2.5-1.0beta3/initdb-mobilitydb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Create the 'mobilitydb' db
+psql <<- 'EOSQL'
+CREATE DATABASE mobilitydb;
+EOSQL
+
+# Create the 'mobilitydb' extension in the mobilitydb database
+echo "Loading MobilityDB extension into mobilitydb"
+psql --dbname="mobilitydb" <<- 'EOSQL'
+	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
+EOSQL
