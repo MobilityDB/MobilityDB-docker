d59313d84b685343e7964af2459dd5e92222b99e
mbakli <mohamed_bakli@aun.edu.eg>
2022-05-27 22:32:52 +0200

    Fix dependencies

--- 11-2.5-1.0beta/initdb-mobilitydb.sh
+++ 11-2.5-1.0beta/initdb-mobilitydb.sh
@@ -5,13 +5,8 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-# Create the 'mobilitydb' db
-psql <<- 'EOSQL'
-CREATE DATABASE mobilitydb;
-EOSQL
-
 # Create the 'mobilitydb' extension in the mobilitydb database
 echo "Loading MobilityDB extension into mobilitydb"
-psql --dbname="mobilitydb" <<- 'EOSQL'
+psql --user="$POSTGRES_USER" --dbname="mobilitydb" <<- 'EOSQL'
 	CREATE EXTENSION IF NOT EXISTS mobilitydb CASCADE;
 EOSQL
